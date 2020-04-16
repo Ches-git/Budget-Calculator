7308240da39177467f52440e255ff6d788f7edbc
Vjaceslavs MArmiss <vjaceslavs.marmiss@gmail.com>
2020-04-16 11:51:29 +0200

    functions

--- MyScript.js
+++ MyScript.js
@@ -1,41 +1,88 @@
-let money = +prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+'use strict';
 
+
+let money, time;
+
+function start() {
+    money = +prompt ("Ваш бюджет на месяц?", "");
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt ("Ваш бюджет на месяц?", ""); 
+    }
+
+}
+start();
+
+    
 let appData = {
-	budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: false
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true
 };
 
 
-
-// Важно! В последних версиях браузеров поведение работы со свойствами объектов поменялось. 
-// Теперь синтаксис через точку может не работать. Поэтому выше я использовал квадратные скобки. 
-// Если вы это читаете - то в скором времени обновится и видео в самом курсе. Спасибо за внимание!
-for(let i = 0; i< 2; i ++){
-	a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	b = prompt("Во сколько обойдется?", '');
-	if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-
-			console.log ("done");
-	
-		appData.expenses[a] = b;
-	} else {
-		i -=1;
-	}
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt ("Во сколько обойдется?", "");
+    
+        if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+            appData.expenses[a] = b;
+        } else {
+            i--;
+        }
+    
+    };
 }
-appData.MoneyPerDay = appData.budget / 30;
-alert("Ваш бюджет :" +  appData.MoneyPerDay);
+chooseExpenses();
 
-if(appData.MoneyPerDay < 100){
-	console.log("минимальный уровень достатка");
-}else if (appData.MoneyPerDay >100 && appData.MoneyPerDay <2000){
-	console.log(`средний уровень достка`);
-} else if  (appData.MoneyPerDay >2000 ){
-	console.log(`хороший уровень достатка`);
-}else{
-	console.log("произошла ошибка");
-	}
+
+function detectDayBudget() {                                            // Расчет дневного бюджета
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+}
+detectDayBudget();
+
+
+function detectLevel() {                                                // Расчет уровня достатка
+    if (appData.moneyPerDay < 100) {
+        console.log ("Это минимальный уровень достатка!");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log ("Это средний уровень достатка!");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log ("Это высокий уровень достатка!");
+    } else {
+        console.log ("Ошибочка...!");
+    }
+}
+detectLevel();
+
+
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+
+            appData.monthIncome = save/100/12*percent;
+            alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
+    }
+}
+checkSavings();
+
+
+function chooseOptExpenses() {                             // Функция для определения необязательных расходов
+
+    for (let i = 1; i <= 3; i++) {
+        let questionOptExpenses = prompt("Статья необязательных расходов?");
+        appData.optionalExpenses[i] = questionOptExpenses;
+        console.log(appData.optionalExpenses);
+    }
+
+
+}
+chooseOptExpenses();
